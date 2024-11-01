1ef9971b9cc4b0476c555ff27b4e379e50dd85a0
artem-spaceto <artem@spaceto.io>
2024-11-01 19:43:32 +0600

    Implemented api-key, updated frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,18 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { join } from 'path'
-import { QrcodeModule } from './qrcode/qrcode.module';
+import { QrcodeModule } from './qrcode/qrcode.module'
+import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
     imports: [
+      ConfigModule.forRoot(),
       ServeStaticModule.forRoot({
         rootPath: join(__dirname, '..', 'public')
       }),
-      QrcodeModule
+      QrcodeModule,
+      AuthModule
     ],
     controllers: [AppController],
     providers: [AppService]
--- src/qrcode/qrcode.controller.ts
+++ src/qrcode/qrcode.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { QrcodeService } from './qrcode.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('qrcode')
 export class QrcodeController {
     constructor(private readonly qrcodeService: QrcodeService) {}
 
     @Get()
+    @UseGuards(AuthGuard('api-key'))
     async getQrCode(@Query('data') data: string) {
         return await this.qrcodeService.generateQrCode(data)
     }
