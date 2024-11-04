efd0b46e9ed1db3359c33552b26eee66335edfa7
artem-spaceto <artem@spaceto.io>
2024-11-04 18:58:25 +0600

    Enable api-key, added swagger

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Header, Param, Post, Query, Req, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Header, Param, Post, Query, Req, Res, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express, Request, Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('image')
 export class ImageController {
     constructor(private readonly imageService: ImageService) {}
 
     @Get()
+    @UseGuards(AuthGuard('api-key'))
     async getImage() {
         let result = await this.imageService.getListObjects()
         return result.Contents
@@ -15,6 +17,7 @@ export class ImageController {
 
     @Post()
     @UseInterceptors(FileInterceptor('file'))
+    @UseGuards(AuthGuard('api-key'))
     async postImage(@Req() req: Request, @UploadedFile() file: Express.Multer.File) {
         let fileName = await this.imageService.uploadObject(file);
 
@@ -25,6 +28,7 @@ export class ImageController {
     }
 
     @Get("qr/:fileName")
+    @UseGuards(AuthGuard('api-key'))
     @Header('Content-Type', 'image/png')
     async getQrCode(@Res() res: Response, @Param('fileName') fileName: string) {
         let fileUrl = `${process.env.S3_ENDPOINT}/${process.env.S3_BUCKET}/${fileName}`
