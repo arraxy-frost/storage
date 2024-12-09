ba69dc67ffbdba475275dfe0a2d734c37d3baac0
Arraxy <artem@spaceto.io>
2024-12-09 17:40:04 +0600

    cors update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            'http://localhost:3000',
+            'https://cola.spaceto.io'
+          ],
+    });
 
     const config = new DocumentBuilder()
         .setTitle('Photo Storage')
